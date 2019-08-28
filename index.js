c21eadb45cd23db5e2c966bf1fc77b46a45fdeb8
Abdulrahman Abuzaid <Abdulrahman.Abuzaid@kaust.edu.sa>
2019-08-28 18:50:06 +0300

    Added a counter to let the user track the number of examples in each class. Some modifications on the styling.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
 let net;
+let count1 = 0, count2 = 0, count3 = 0;
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -21,19 +22,32 @@ async function app() {
   };
 
   // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-a').addEventListener('click', () => {
+    addExample(0);
+    count1++;
+  }
+  );
+  document.getElementById('class-b').addEventListener('click', () => {
+    addExample(1);
+    count2++}
+  );
+  document.getElementById('class-c').addEventListener('click', () => {
+    addExample(2);
+    count3++}
+  );;
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
-      // Get the most likely class and confidences from the classifier module.
+      // Get the most likely class and confidences from t5he classifier module.
       const result = await classifier.predictClass(activation);
 
       const classes = ['A', 'B', 'C'];
       document.getElementById('console').innerText = `
+        Count of Class A: ${count1}
+        Count of Class B: ${count2}
+        Count of Class C: ${count3}\n
         prediction: ${classes[result.classIndex]}\n
         probability: ${result.confidences[result.classIndex]}
       `;
